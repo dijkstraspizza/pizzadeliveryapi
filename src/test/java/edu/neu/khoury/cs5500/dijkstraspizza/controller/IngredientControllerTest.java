94097e22ec9ad50f0d2ba0ca09b4e799b7e92e2c
Josh Baron <baron.jos@husky.neu.edu>
2019-11-01 13:20:02 -0700

    removed unused imports

--- src/test/java/edu/neu/khoury/cs5500/dijkstraspizza/controller/IngredientControllerTest.java
+++ src/test/java/edu/neu/khoury/cs5500/dijkstraspizza/controller/IngredientControllerTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;\
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.*;
 import java.util.stream.Collectors;
