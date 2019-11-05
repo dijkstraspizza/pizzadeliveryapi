66f89ff4b97864adef0a02e31b51c2fd1390d4b2
Josh Baron <baron.jos@husky.neu.edu>
2019-11-05 13:09:14 -0800

    fixed PriceCalculator

--- src/main/java/edu/neu/khoury/cs5500/dijkstraspizza/model/PriceCalculator.java
+++ src/main/java/edu/neu/khoury/cs5500/dijkstraspizza/model/PriceCalculator.java
@@ -10,88 +10,49 @@ import java.util.List;
 @Data
 public class PriceCalculator {
 
+
+  @Id
+  private String id;
   private Double discountRatio;
   private Integer requiredPizzas;
-  private Integer baseFreeIngredients;
   private Integer pizzasAppliedTo;
 
   public PriceCalculator() {
     this.requiredPizzas = 0;
     this.pizzasAppliedTo = -1;
     this.discountRatio = 0.0;
-    this.baseFreeIngredients = 0;
-  }
-
-  public PriceCalculator(Integer baseFreeIngredients) {
-    this.requiredPizzas = 0;
-    this.pizzasAppliedTo = -1;
-    this.discountRatio = 0.0;
-    this.baseFreeIngredients = baseFreeIngredients;
   }
 
   public PriceCalculator(Double discountRatio) {
     this.requiredPizzas = 0;
     this.pizzasAppliedTo = -1;
     this.discountRatio = discountRatio;
-    this.baseFreeIngredients = 0;
   }
 
-  public PriceCalculator(Integer baseFreeIngredients, Double discountRatio) {
-    this.discountRatio = discountRatio;
-    this.requiredPizzas = 0;
-    this.pizzasAppliedTo = -1;
-    this.baseFreeIngredients = baseFreeIngredients;
-  }
-
-  public PriceCalculator(Integer baseFreeIngredients, Integer requiredPizzas,
-                              Integer pizzasAppliedTo, Double discountRatio) {
+  public PriceCalculator(Integer requiredPizzas, Integer pizzasAppliedTo, Double discountRatio) {
     this.requiredPizzas = requiredPizzas;
     this.pizzasAppliedTo = pizzasAppliedTo;
     this.discountRatio = discountRatio;
-    this.baseFreeIngredients = baseFreeIngredients;
   }
 
-  public static Double calculatePizzaPrice(Double pizzaPrice,
-                                           Integer baseFreeIngredients,
-                                           List<Ingredient> ingredients) {
-    double price = pizzaPrice;
-    for (int i = baseFreeIngredients; i < ingredients.size(); i++) {
-      price += ingredients.get(i).getPrice();
-    }
-    return price;
+  private Double calculateBasePrice(List<Pizza> pizzas) {
+    return pizzas.stream().mapToDouble(Pizza::getPrice).sum();
   }
 
-  private Double calculateBasePrice(List<Double> pizzaPrices,
-                                    List<List<Ingredient>> pizzaIngredients) {
-    double price = 0;
-    for (int i = 0; i < pizzaPrices.size(); i++) {
-      price += calculatePizzaPrice(pizzaPrices.get(i), baseFreeIngredients, pizzaIngredients.get(i));
+  public Double calculate(List<Pizza> pizzas) {
+    if (pizzas.size() < requiredPizzas) {
+      return calculateBasePrice(pizzas);
     }
-    return price;
-  }
-
-  @Id
-  private String id;
-
-  public Double calculate(List<Double> pizzaPrices,
-                          List<List<Ingredient>> pizzaIngredients) {
-    if (pizzaPrices.size() < requiredPizzas) {
-      return calculateBasePrice(pizzaPrices, pizzaIngredients);
-    }
-    if (pizzasAppliedTo < 0) {
-      double price = calculateBasePrice(pizzaPrices, pizzaIngredients);
+    if (pizzasAppliedTo < 0) { // -1 represents all pizzas
+      double price = calculateBasePrice(pizzas);
       return price - price * discountRatio;
     }
     double price = 0;
-    double pizzaPrice;
     for (int i = 0; i < pizzasAppliedTo; i++) {
-      pizzaPrice = calculatePizzaPrice(pizzaPrices.get(i), baseFreeIngredients,
-          pizzaIngredients.get(i));
-      price += pizzaPrice - discountRatio * pizzaPrice;
+      price += pizzas.get(i).getPrice() - discountRatio * pizzas.get(i).getPrice();
     }
-    for (int i = pizzasAppliedTo; i < pizzaPrices.size(); i++) {
-      price += calculatePizzaPrice(pizzaPrices.get(i),
-          baseFreeIngredients, pizzaIngredients.get(i));
+    for (int i = pizzasAppliedTo; i < pizzas.size(); i++) {
+      price += pizzas.get(i).getPrice();
     }
     return price;
   }
