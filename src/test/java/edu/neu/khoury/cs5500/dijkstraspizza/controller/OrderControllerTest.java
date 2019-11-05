48ad47973f38a97e70591284669ce4e696d95cc5
Josh Baron <baron.jos@husky.neu.edu>
2019-11-05 15:27:26 -0800

    OrderControllerTest

--- src/test/java/edu/neu/khoury/cs5500/dijkstraspizza/controller/OrderControllerTest.java
+++ src/test/java/edu/neu/khoury/cs5500/dijkstraspizza/controller/OrderControllerTest.java
@@ -27,7 +27,6 @@ import java.util.Optional;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(OrderController.class)
