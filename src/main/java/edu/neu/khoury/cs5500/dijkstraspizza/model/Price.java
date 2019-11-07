57f37af551102391c75dc8a12471e592e216a63b
Josh Baron <baron.jos@husky.neu.edu>
2019-11-06 20:51:11 -0800

    PriceCalculatorController::getOrderPrice now returns Price object

---
+++ src/main/java/edu/neu/khoury/cs5500/dijkstraspizza/model/Price.java
@@ -0,0 +1,13 @@
+package edu.neu.khoury.cs5500.dijkstraspizza.model;
+
+public class Price {
+  private double price;
+
+  public Price(double price) {
+    this.price = price;
+  }
+
+  public Double get() {
+    return price;
+  }
+}
