8f3a505225316eb21bad03dd97c083e0361df124
Josh Baron <baron.jos@husky.neu.edu>
2019-11-07 16:48:58 -0800

    PizzaSizeController tests

--- src/main/java/edu/neu/khoury/cs5500/dijkstraspizza/model/PizzaSize.java
+++ src/main/java/edu/neu/khoury/cs5500/dijkstraspizza/model/PizzaSize.java
@@ -1,6 +1,10 @@
 package edu.neu.khoury.cs5500.dijkstraspizza.model;
 
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "sizes")
 public class PizzaSize {
 
   private static final double SMALL = 8.0;
@@ -10,6 +14,8 @@ public class PizzaSize {
   private static final String MEDIUM_DESCRIPTION = "medium";
   private static final String LARGE_DESCRIPTION = "large";
 
+  @Id
+  private String id;
   private double inches;
   private String description;
   private double value;
@@ -54,5 +60,25 @@ public class PizzaSize {
 
   public double getValue() {
     return value;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public void setInches(double inches) {
+    this.inches = inches;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public void setValue(double value) {
+    this.value = value;
   }
 }
