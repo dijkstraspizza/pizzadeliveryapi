06a6bf034a83707eeac2c851e128424a7ed8d3d5
Josh Baron <baron.jos@husky.neu.edu>
2019-11-12 13:50:17 -0800

    added CardValidator to OrderValidator

--- src/main/java/edu/neu/khoury/cs5500/dijkstraspizza/controller/validator/OrderValidator.java
+++ src/main/java/edu/neu/khoury/cs5500/dijkstraspizza/controller/validator/OrderValidator.java
@@ -1,5 +1,6 @@
 package edu.neu.khoury.cs5500.dijkstraspizza.controller.validator;
 
+import edu.neu.khoury.cs5500.dijkstraspizza.model.CreditCardInfo;
 import edu.neu.khoury.cs5500.dijkstraspizza.model.Order;
 import edu.neu.khoury.cs5500.dijkstraspizza.model.Pizza;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,27 @@ public class OrderValidator implements Validator<Order> {
   @Autowired
   PizzaValidator pizzaValidator;
 
+  @Autowired
+  CreditCardValidator cardValidator;
+
   @Override
   public boolean validate(Order entity) {
-    return validatePizzas(entity.getPizzas());
+    return validateCard(entity.getCardInfo()) && validatePizzas(entity.getPizzas());
+  }
+
+  private boolean validateCard(CreditCardInfo cardInfo) {
+    if (cardInfo == null) {
+      return false;
+    }
+    return cardValidator.validate(cardInfo);
   }
 
   private boolean validatePizzas(List<Pizza> pizzas) {
+
     for (Pizza pizza : pizzas) {
+      if (pizza == null) {
+        return false;
+      }
       if (!pizzaValidator.validate(pizza)) {
         return false;
       }
