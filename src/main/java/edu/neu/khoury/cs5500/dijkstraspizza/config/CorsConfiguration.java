6ae4df07e6242fa74df71762e377266cac58feca
Evan <EvanLDouglass@gmail.com>
2019-11-20 13:57:32 -0800

    Revert Changes

---
+++ src/main/java/edu/neu/khoury/cs5500/dijkstraspizza/config/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package edu.neu.khoury.cs5500.dijkstraspizza.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfiguration {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE");
+      }
+    };
+  }
+}
